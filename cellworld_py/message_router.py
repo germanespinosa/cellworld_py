b98829d3e28ed304a0dd079ea1daa2d013517812
German Espinosa <germanespinosa@gmail.com>
2021-11-20 19:22:21 -0600

    version 1.1

--- cellworld_py/message_router.py
+++ cellworld_py/message_router.py
@@ -10,18 +10,21 @@ class Message_router:
         self.unrouted_message = None
         self.processing = False
 
-    def add_route(self, pattern, handler):
+    def add_route(self, pattern, handler, body_type=None):
         check_type(pattern, str, "incorrect type for pattern")
         check_types(handler, [types.FunctionType, types.MethodType], "incorrect type for handler")
-        self.routes[pattern] = handler
+        self.routes[pattern] = (handler, body_type)
 
     def route(self, message):
         responses = []
         check_type(message, Message, "incorrect type for message")
         for pattern in self.routes.keys():
             if re.search(pattern, message.header):
-                handler = self.routes[pattern]
-                responses.append(handler(message))
+                (handler, body_type) = self.routes[pattern]
+                if body_type:
+                    responses.append(handler(message.get_body(body_type)))
+                else:
+                    responses.append(handler(message))
         if not responses:
             if self.unrouted_message:
                 self.unrouted_message(message)
