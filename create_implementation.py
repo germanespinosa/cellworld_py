9670416abc64b117c3a97cf1ea048640e5954c11
German Espinosa <germanespinosa@gmail.com>
2021-11-23 15:26:03 -0600

    version 1.2

--- create_implementation.py
+++ create_implementation.py
@@ -12,16 +12,5 @@ new_space = Space(Location(center_x, center_y), Shape(6), Transformation(size,ro
 
 new_implementation = World_implementation.get_from_name("hexagonal", "canonical")
 new_implementation.transform(new_space)
-
-#
-# new_implementation.space.transformation.size *= size
-# new_implementation.space.center.x *= center_x
-# new_implementation.space.center.y *= center_y
-# new_implementation.cell_transformation.size *= size
-#
-# for cell_location in new_implementation.cell_locations:
-#     cell_location.x = (cell_location.x-.5) * size + center_x
-#     cell_location.y = (cell_location.y-.5) * size + center_y
-#
-with open("hexagonal."+implementation_name,"w") as f:
+with open("hexagonal."+implementation_name, "w") as f:
     f.write(str(new_implementation))
